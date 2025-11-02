867fbb5ca61e856f57f2bc765e8f240211cf34f4
linhntt317 <72378932+linhntt317@users.noreply.github.com>
2025-11-02 22:49:40 +0700

    Updated PostTruyen.tsx

--- src/pages/admin/PostTruyen.tsx
+++ src/pages/admin/PostTruyen.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
